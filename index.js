748183f14330e7676f8a8333fb00d7e3d6d17732
Justin Gorham <justingorham@gmail.com>
2022-07-30 17:11:00 -0400

    project 2 scaffold

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-const http = require('http')
-const finalhandler = require('finalhandler')
-const serveStatic = require('serve-static')
-
-const serve = serveStatic('./src', { index: ['index.html', 'index.htm'] })
-
-// Create server
-var server = http.createServer(function onRequest(req, res) {
-    serve(req, res, finalhandler(req, res))
-})
-
-// Listen
-server.listen(3000)
+module.exports = {}
